2faee7e3eaea6537bc3cb190ca3e494eb30d50be
boingy102 <martin.mekota@gmail.com>
2014-02-17 18:07:12 +0100

    Druhy

---
+++ src/exception/SpojovySeznam.java
@@ -0,0 +1,19 @@
+package exception;
+
+import java.util.ArrayList;
+
+public class SpojovySeznam {
+	static ArrayList<Object> zoznam = new ArrayList<Object>();
+	
+	void vloz(Object obj) {
+		zoznam.add(obj);
+	}
+	
+	void vypisZoznam() {
+		for(int i = 0; i < zoznam.size(); i++) {
+			System.out.println(zoznam.get(i));
+			System.out.println("Nieco...");
+
+		}
+	}
+}
